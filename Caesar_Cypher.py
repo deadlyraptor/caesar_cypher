2a86ff5198dea25d318532b31684b7a9f9716f1e
Javier <jachavez6@gmail.com>
2013-09-08 23:45:59 -0400

    third commit

--- Caesar_Cypher.py
+++ Caesar_Cypher.py
@@ -1,9 +1,12 @@
+import re
+
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 o_phrase = raw_input('Phrase to encrypt: ')
+has_char = re.search('[A-Za-z]', o_phrase)
 
-if len(o_phrase) > 0:
+if has_char:
         e_phrase = ''
         for letter in o_phrase.upper():
                 if letter not in alphabet:
@@ -14,11 +17,3 @@ if len(o_phrase) > 0:
         print e_phrase
 else:
 	print 'That is not a valid word or phrase. Please try again.'
-
-
-"""
-Future Features:
-
-1. Decryption.
-
-"""
